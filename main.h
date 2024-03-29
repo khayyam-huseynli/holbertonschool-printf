fa3a2410fc38c9a613440832bb85d6bd43968b35
Raid-dev <a_rauf2004@mail.ru>
2024-03-29 05:52:30 +0000

    added prototype for _puts func

--- main.h
+++ main.h
@@ -18,13 +18,15 @@ typedef struct print_func
 int _printf(const char *format, ...);
 int get_print_func(char ch, va_list ap);
 
-int _putchar(char c);
+/** print functions */
 int print_char(va_list ap);
 int print_string(va_list ap);
 int print_int(va_list ap);
 int print_binary(va_list ap);
 
 /** helper functions */
+int _putchar(char c);
+int _puts(char *str);
 void print_number(int n);
 int count_digit(int i);
 char *convert(unsigned long int num, int base, int lowercase);

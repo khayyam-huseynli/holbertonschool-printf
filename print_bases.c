7b518bd28717b47014bdfe4ce8760fa1d2439a73
khayyam <khayyam.huseynli@gmail.com>
2024-03-29 06:35:03 +0000

    adde func for handling o format spec

--- print_bases.c
+++ print_bases.c
@@ -14,3 +14,21 @@ int print_binary(va_list ap)
 
 	return (_puts(str));
 }
+
+
+/**
+ * print_octal - prints a number in base 8
+ * @ap: va_list arguments from _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_octal(va_list ap)
+{
+	unsigned int num = va_arg(ap, unsigned int);
+	char *str = convert(num, 8, 0);
+	int count = 0;
+
+	count += _puts(str);
+	return (count);
+}

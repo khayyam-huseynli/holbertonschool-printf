25b9953c79de34b9451b13e496f9cd85da942b1f
khayyam <khayyam.huseynli@gmail.com>
2024-03-29 07:36:29 +0000

    added element for u format spec

--- get_print_func.c
+++ get_print_func.c
@@ -17,6 +17,7 @@ int get_print_func(char ch, va_list ap)
 	{"d", print_int},
 	{"i", print_int},
 	{"b", print_binary},
+	{"u", print_unsigned},
 	{"o", print_octal},
 	{"x", print_hex},
 	{"X", print_hex_big},

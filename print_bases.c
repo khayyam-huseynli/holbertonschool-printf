74948595fe3d3798d89964323a924cecbb11bd4c
khayyam <khayyam.huseynli@gmail.com>
2024-03-29 06:52:57 +0000

    adde func for handling x and X format spec

--- print_bases.c
+++ print_bases.c
@@ -32,3 +32,40 @@ int print_octal(va_list ap)
 	count += _puts(str);
 	return (count);
 }
+
+
+/**
+ * print_hex - prints a number in hexadecimal base,
+ *             in lowercase
+ * @ap: va_list arguments from _printf
+ * Description: the function calls convert() which in turns converts the input
+ *              number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_hex(va_list ap)
+{
+	unsigned int num = va_arg(ap, unsigned int);
+	char *str = convert(num, 16, 1);
+	int count = 0;
+
+	count += _puts(str);
+	return (count);
+}
+
+/**
+ * print_hex_big - prints a number in hexadecimal base,
+ *                 in uppercase
+ * @ap: va_list arguments from _printf
+ * Description: the function calls convert() which in turns converts the input
+ *              number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_hex_big(va_list ap)
+{
+	unsigned int num = va_arg(ap, unsigned int);
+	char *str = convert(num, 16, 0);
+	int count = 0;
+
+	count += _puts(str);
+	return (count);
+}

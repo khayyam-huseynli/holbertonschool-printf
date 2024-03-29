2542ecf4f8c54019eb31cf447b120bf3d8265eb5
Raid-dev <a_rauf2004@mail.ru>
2024-03-29 09:38:56 +0000

    modified _putchar func so that it uses a local buffer of 1024 to call write as little as possible

--- print_alpha.c
+++ print_alpha.c
@@ -6,11 +6,26 @@
  * @c: The character to print
  * Return: 1 on SUCCESS,
  *	   -1 on error and errno is set appropriately.
+ * Description: _putchar uses a local buffer of 1024 to call write
+ * as little as possible
  */
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
 }
 
 /**

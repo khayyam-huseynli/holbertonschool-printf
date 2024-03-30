00c8dce1c558f924fbbc5cf266406e3f6c4d4ae1
khayyam <khayyam.huseynli@gmail.com>
2024-03-30 18:18:27 +0400

    corrected return

--- get_print_func.c
+++ get_print_func.c
@@ -28,12 +28,12 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'p', print_address},
 		{'%', print_percent}
 		};
-	int flags = 14;
 
-	register int i;
+	int i = 0;
 
-	for (i = 0; i < flags; i++)
-		if (func_arr[i].format == s)
-			return (func_arr[i].f);
-	return ((int (*)(__va_list_tag *, flags_t *))NULL);
+	while (func_arr[i].format != NULL && *(func_arr[i].format) != s)
+	{
+		i++;
+	}
+	return (func_arr[i].f);
 }

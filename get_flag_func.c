5ac4b984822d89e339be5441f3849ba1666a2880
khayyam <khayyam.huseynli@gmail.com>
2024-03-31 17:44:59 +0400

    removed all related l,h,width,0,- flags

--- get_flag_func.c
+++ get_flag_func.c
@@ -26,22 +26,6 @@ int get_flag(char s, flags_t *f)
 			f->hash = 1;
 			i = 1;
 			break;
-		case '0':
-			f->zero = 1;
-			i = 1;
-			break;
-		case '-':
-			f->minus = 1;
-			i = 1;
-			break;
-		case 'l':
-			f->l_long = 1;
-			i = 1;
-			break;
-		case 'h':
-			f->h_short = 1;
-			i = 1;
-			break;
 	}
 
 	return (i);
